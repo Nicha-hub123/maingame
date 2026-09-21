e209e06d45648dee12a1fb17aa6feeb805a0b122
Nicha-hub123 <nichaisgood123@gmail.com>
2026-09-21 22:11:30 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local scripts = {
     [115942182868656] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Slimeoutfish.lua",
     [98502499119821] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Heavyweightfishing.lua",   
     [80158232099900] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Holefishing.lua", 
+    [113945994875620] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Slimeoutfish.lua",
 }
 
 if scripts[placeId] then
