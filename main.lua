d24d9c2bdc2e32f895676ee7649b4993cae3f80b
Nicha-hub123 <nichaisgood123@gmail.com>
2026-09-21 09:52:23 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 local scripts = {
     [113945994875620] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Slimeoutfish.lua",
-    [1234567890] = "https://raw.githubusercontent.com/USER/REPO/main/fishgame.lua",   
+    [98502499119821] = "https://raw.githubusercontent.com/Nicha-hub123/maingame/refs/heads/main/Heavyweightfishing.lua",   
     [9876543210] = "https://raw.githubusercontent.com/USER/REPO/main/kinglegacy.lua", 
 }
 
